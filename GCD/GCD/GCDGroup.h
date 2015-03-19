5f8ef88e8a14e9cd0f33dad500640d02e85691b2
王钱钧 <wangqianjun168@163.com>
2015-03-19 11:14:18 +0800

    添加dispatch group

---
+++ GCD/GCD/GCDGroup.h
@@ -0,0 +1,13 @@
+//
+//  GCDGroup.h
+//  GCD
+//
+//  Created by 王钱钧 on 15/3/19.
+//  Copyright (c) 2015年 Arthur. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCDGroup : NSObject
+- (void)testDispatchGroup;
+@end
