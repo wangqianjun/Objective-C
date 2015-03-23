92d144105f8c6985a1fa559f8e64babc38ac01f4
王钱钧 <wangqianjun168@163.com>
2015-03-23 14:49:35 +0800

    添加ShapeLayer的学习代码

---
+++ CoreAnimation/CAShapeLayer/CAShapeLayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CAShapeLayer
+//
+//  Created by 王钱钧 on 15/3/23.
+//  Copyright (c) 2015年 Arthur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreAnimation/CAShapeLayer/CAShapeLayer/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  CAShapeLayer
+//
+//  Created by 王钱钧 on 15/3/23.
+//  Copyright (c) 2015年 Arthur. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (strong, nonatomic) UIBezierPath *cyclePath;
+//@property (strong, nonatomic)
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
